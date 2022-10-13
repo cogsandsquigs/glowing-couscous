bf38ab2e2c3c2580c0f76c8ab71b79a326b88cb9
Ian Pratt <ianjdpratt@gmail.com>
2022-10-12 21:20:41 -0500

    asdfgwr4g

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# jsfuck
+# jsfuck-rust
+
 An implementation of [JSFuck](https://en.wikipedia.org/wiki/JSFuck) written in rust.
 
 ## Why?
+
 I was bored. I saw this online, written in JS. Rust, being the obviously superior alternative, should have been employed. I am here to do that
 
 ## But others have done this before
+
 "bUt OtHeRs HaVe" I dont care. This is my silly little project, just for fun and practice.
--- src/fuck.rs
+++ src/fuck.rs
@@ -4,7 +4,7 @@ const ZERO: &str = "+[]";
 /// Encodes one. Works because `!` coerces `[]` into a boolean type (`false`),
 /// and then the next `!` coerces it into `true`. The final `+` coerces it
 /// into an integer type, which is `1`, as the value is `true`.
-const ONE: &str = "+!![]";
+const ONE: &str = " +!![] ";
 
 /// Encodes true. Works becuase `+[]` is zero, and `!` coerces it into `true`
 /// (as `0` is `false`, so `!0` is `true`).
@@ -16,32 +16,47 @@ const FALSE: &str = "![]";
 /// Encodes any number into a JSFuck number
 pub fn encode_number(n: isize) -> String {
     if n == 0 {
-        ZERO.into()
+        ZERO.into() // just return zero
     } else if n == 1 {
-        ONE.into()
-    } else if n > 1 {
+        ONE.into() // just return one
+    } else {
         format!(
             "{} + {}",
             ONE,
             (" + ".to_string() + ONE).repeat((n - 1) as usize)
-        )
-    } else if n == -1 {
-        format!("-({})", ONE)
-    } else {
-        format!(
-            "-({} + {})",
-            ONE,
-            (" + ".to_string() + ONE).repeat(((-n) - 1) as usize)
         )
     }
 }
 
 /// Encodes an arbitrary character into a JSFuck character
 pub fn encode_character(c: char) -> String {
-    // We first need to get the characters
-    // c, o, n, s, t, r, u, f, o, m, C, h, a, d, e
+    // We first need to get the characters to make `
+    // c, o, t, r, u, m, C, h, d
     match c {
-        'a' => "(![]+[])[+!+[]]".into(),
+        'a' => format!("(![]+[])[{}]", encode_number(1)),
+        'e' => format!("(![]+[])[{}]", encode_number(4)),
+        'f' => format!("(![]+[])[{}]", encode_number(0)),
+        'i' => format!(
+            "([]+ +(+!+[]+(!+[]+[])[!+[]+!+[]+!+[]]+[+!+[]]+[+[]]+[+[]]+[+[]]))[{}]",
+            encode_number(3)
+        ),
+        'l' => format!("(![]+[])[{}]", encode_number(2)),
+        'n' => format!(
+            "([]+ +(+!+[]+(!+[]+[])[!+[]+!+[]+!+[]]+[+!+[]]+[+[]]+[+[]]+[+[]]))[{}]",
+            encode_number(1)
+        ),
+        'r' => format!("(!+[]+[])[{}]", encode_number(1)),
+        's' => format!("(![]+[])[{}]", encode_number(3)),
+        't' => format!("(!+[]+[])[{}]", encode_number(0)),
         _ => todo!(),
     }
+}
+
+/// Encodes an arbitrary string into JSFuck characters.
+/// Note: returns js string type.
+pub fn encode_string(s: &str) -> String {
+    s.chars() //gets the characters
+        .map(encode_character) // maps them into jsfuck chars
+        .collect::<Vec<String>>() // collects them for the next part
+        .join("+") // joins and concatenates them as a string
 }
