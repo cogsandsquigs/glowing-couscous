be4587043babe62efb98e062f2e776cad096dc68
Ian Pratt <ianjdpratt@gmail.com>
2022-10-14 17:58:30 -0500

    yaaaay its complete

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ I was bored. I saw this online, written in JS. Rust, being the obviously superio
 ## But others have done this before
 
 "bUt OtHeRs HaVe" I dont care. This is my silly little project, just for fun and practice.
+
+## How to use
+
+Just run `jarfe -h` to see the help message. It will tell you how to use it. It's pretty self-explanatory.
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@ use clap::Parser;
 use std::fs::File;
 use std::io::prelude::*;
 
-/// Simple program to greet a person
+/// Translates a regular javascript program into a JSFuck program,
+/// a valid (!) program that only uses the characters +, !, [, ],
+/// (, and ).
 #[derive(Clone, Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// The filename to parse the javascript from
-    #[arg(short, long)]
+    // #[arg(short, long)]
     filename: String,
 
     /// The output file
-    #[arg(short, long, default_value = "jsfuck.js")]
+    #[arg(short, long, default_value = "fuck.js")]
     output: String,
 }
 
@@ -30,5 +32,11 @@ fn main() {
         .expect("Error while reading file");
 
     let obfusicated = obfusicate(js.as_str());
-    println!("{}", obfusicated);
+
+    let mut output = File::create(args.output.clone())
+        .expect(format!("Error while creating file `{}`", args.output).as_str());
+
+    output
+        .write(obfusicated.as_bytes())
+        .expect(format!("Error while writing to file `{}`", args.output).as_str());
 }
